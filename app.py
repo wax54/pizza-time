a7aef04f8738d4f10f792069c9c498fcddea97f1
sam <wax654@gmail.com>
2021-04-25 21:56:57 -0700

    Updated order card look

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, redirect
 from config import DB_URL, SECRET_KEY
 from db_setup import connect_db, db
 from users.__init__ import auth_views, user_views
-
+import datetime
 from deliveries.models import *
 
 app = Flask(__name__)
@@ -20,9 +20,14 @@ connect_db(app)
 # db.drop_all()
 # db.create_all()
 
-""" taken from 
-https://coderwall.com/p/4zcoxa/urlencode-filter-in-jinja2
-"""
+
+@app.template_filter('dateformat')
+def format_date(value, format='medium'):
+    if format == 'full':
+        format = "%A, %B %d"
+    elif format == 'medium':
+        format = "%a, %b %d"
+    return datetime.date.strftime(value, format)
 
 app.register_blueprint(auth_views, url_prefix="")
 app.register_blueprint(user_views, url_prefix="")
