bed719182c12926d9e2ea8a2991bcdbbed22ae5f
sam <wax654@gmail.com>
2021-08-13 15:43:25 -0700

    added comment for clarity

--- static/map_links.js
+++ static/map_links.js
@@ -5,6 +5,7 @@ function replaceMapLinksForIphone() {
         (navigator.platform.indexOf("iPod") != -1)){
             const mapLinks = document.getElementsByClassName("map-link");
             for(let link of mapLinks){
+                //replace all the links with map links
                 const ref = link.href;
                 link.href = ref.replace("https://", "maps://");
             }
