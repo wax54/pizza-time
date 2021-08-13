e0d6f58c5e4dfe18c9e1d5e5831477d4ebf2e091
sam <wax654@gmail.com>
2021-08-13 15:41:23 -0700

    Added working app map links for iphon

---
+++ static/map_links.js
@@ -0,0 +1,14 @@
+function replaceMapLinksForIphone() {
+    if /* if we're on iOS, open in Apple Maps */
+        ((navigator.platform.indexOf("iPhone") != -1) ||
+        (navigator.platform.indexOf("iPad") != -1) ||
+        (navigator.platform.indexOf("iPod") != -1)){
+            const mapLinks = document.getElementsByClassName("map-link");
+            for(let link of mapLinks){
+                const ref = link.href;
+                link.href = ref.replace("https://", "maps://");
+            }
+        }
+}
+
+replaceMapLinksForIphone();
